61502454130a93751ccab8cfb9c1df3f1309a24d
wachiye wanyama <wachiye@wachiyes-MacBook-Pro.local>
2023-05-11 00:49:29 +0300

    basic setup for web, android and IOs

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { NativeBaseProvider, Box } from "native-base";
+
+export default function App() {
+  return (
+      <NativeBaseProvider>
+        <Box>Open up App.js to start working on your app!</Box>
+      </NativeBaseProvider>
+  );
+}
